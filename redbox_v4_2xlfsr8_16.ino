0dd5724e0f9179c2115f6635ffd949dbbe6aa422
Richard Jelbert <richard@jelbert.com>
2020-03-21 20:54:37 +0000

    adding the random jitter back in

--- redbox_v4_2xlfsr8_16.ino
+++ redbox_v4_2xlfsr8_16.ino
@@ -162,8 +162,8 @@ byte bb = 0;
 byte dly = 0;
 
 // add a random delay to create timimg jitter
-//dly = (PIND & 0b00111100) >> 2; // 0 to 15
-//delayMicroseconds(dly + 1);
+dly = (PIND & 0b00111100) >> 2; // 0 to 15
+delayMicroseconds(dly + 1);
 
 delayMicroseconds(10);
     aa = PIND & 0b00111100;
