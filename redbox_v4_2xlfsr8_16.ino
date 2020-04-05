467b1c8b5ce75c4b85150b57304e091a04b24ee9
Richard Jelbert <richard@jelbert.com>
2020-04-05 12:21:57 +0100

    Update redbox_v4_2xlfsr8_16.ino
    updating the nibble read code to add more jitter

--- redbox_v4_2xlfsr8_16.ino
+++ redbox_v4_2xlfsr8_16.ino
@@ -162,7 +162,7 @@ byte bb = 0;
 byte dly = 0;
 
 // add a random delay to create timimg jitter
-dly = (PIND & 0b00111100) >> 2; // 0 to 15
+dly = (PIND & 0b00111100) << 1; // 0 to 127 with steps!!
 // delayMicroseconds(dly + 1);
 for (int i = 0;i<=dly;i++) { }
   
