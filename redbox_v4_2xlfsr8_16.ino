a5341d6f81c87a3a584141c916db616b58102719
Richard Jelbert <richard@jelbert.com>
2020-04-23 13:47:58 +0100

    Update redbox_v4_2xlfsr8_16.ino
    updating references to 9000 noise counts

--- redbox_v4_2xlfsr8_16.ino
+++ redbox_v4_2xlfsr8_16.ino
@@ -212,10 +212,10 @@ if (psu_avr < 498) { flash(6); }
 if (psu_delta > 25) { flash(7); }
 }
  
-// check entropy channel frequencies (ideal >8000)
-// 8000 is not the real count, only what the Arduino "sees" in the loop. Real value is much higher
+// check entropy channel frequencies (ideal >9000)
+// 9000 is not the real count, only what the Arduino "sees" in the loop. Real value is much higher
 
-unsigned long lowest = 7100;
+unsigned long lowest = 8000;
 
 // check channel 1 (d2)
 counter =0;
