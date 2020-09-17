ebd76a48d0ab7b7d75e9004af2f33c1c9121fea4
Richard Jelbert <richard@jelbert.com>
2020-09-17 08:18:15 +0100

    Update redbox_v4_2xlfsr8_16.ino
    making the psu delta check a little more relaxed going from 25 to 35 to allow redbox to work with switched mode PSU. Will need to monitor how this might impact quality because the rule should be always use a high quality 12v PSU with low noise

--- redbox_v4_2xlfsr8_16.ino
+++ redbox_v4_2xlfsr8_16.ino
@@ -188,7 +188,7 @@ int psu_delta;
 
 psu_delta = 1000;
 psu_avr = 1000;
-while (psu_delta > 25 || psu_avr < 480 || psu_avr > 512){
+while (psu_delta > 35 || psu_avr < 480 || psu_avr > 512){
 starttime = millis();
 endtime = starttime;
 counter = 0;
@@ -209,7 +209,7 @@ psu_delta = psu_max - psu_min;
 //Serial.println(psu_avr);
 if (psu_avr > 512) { flash(5); }
 if (psu_avr < 480) { flash(6); }
-if (psu_delta > 25) { flash(7); }
+if (psu_delta > 35) { flash(7); }
 }
  
 // check entropy channel frequencies (ideal >=10000)
